b6a6d1f36020d72711f7c11b8cae69829f6522c7
Thiago <thiivieira@gmail.com>
2022-02-10 12:49:17 -0300

    Requisito 2 e 4

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,6 @@
 SELECT 
     COUNT(c.nome) AS cancoes,
     (SELECT COUNT(id) FROM SpotifyClone.artistas) AS artistas,
-    (SELECT COUNT(nome) FROM SpotifyClone.album) AS album
+    (SELECT COUNT(nome) FROM SpotifyClone.album) AS albuns
 FROM
     SpotifyClone.musicas AS c;
